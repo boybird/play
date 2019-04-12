4a76b11ca2a53621d49ff5c502c0bfb63c7d1d54
zxk <zxk7516@foxmail.com>
2019-04-12 17:29:01 +0800

    DD

---
+++ examples/ws.rs
@@ -0,0 +1,45 @@
+use actix::{Actor, StreamHandler};
+use actix_web::{server, ws, App};
+#[macro_use]
+extern crate failure;
+
+struct Room {
+    name: String,
+}
+#[derive(Debug, Fail)]
+enum SystemError {
+    #[fail(display = "ws:{}", _0)]
+    WsProtocolErr(#[fail(cause)] ws::ProtocolError),
+    #[fail(display = "io:{}", _0)]
+    IoError(#[fail(cause)] std::io::Error),
+}
+
+impl std::convert::From<std::io::Error> for SystemError {
+    fn from(e: std::io::Error) -> SystemError {
+        SystemError::IoError(e)
+    }
+}
+
+struct Ws;
+
+impl Actor for Ws {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for Ws {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        match msg {
+            ws::Message::Ping(msg) => ctx.pong(&msg),
+            ws::Message::Text(text) => ctx.text(text),
+            _ => (),
+        }
+    }
+}
+
+fn main() -> Result<(), SystemError> {
+    server::new(|| App::new().resource("/ws/", |r| r.f(|req| ws::start(req, Ws))))
+        .bind("0.0.0.0:1257")?
+        .run();
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,64 @@
 use actix_web::{http, middleware, server, App};
 
 mod routes;
-fn main() {
+fn main2() {
     server::new(|| {
         App::new()
-             .middleware(middleware::Logger::default())
+            .middleware(middleware::Logger::default())
             .configure(routes::config)
         //.resource("/",|r| r.method(http::Method::GET).f(routes::index))
         //.resource("blog", |r| r.method(http::Method::GET).f(routes::blog))
     })
     .bind("127.0.0.1:8079")
-    .unwrap().run();
-    
+    .unwrap()
+    .run();
+}
+
+//extern crate serde;
+//extern crate serde_json;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MyMap {
+    #[serde(rename = "innerMap")]
+    inner_map: HashMap<i32, String>,
+}
+
+#[macro_use]
+extern crate failure;
+#[derive(Debug, Fail)]
+enum MainError {
+    #[fail(display = "serde error:{}", _0)]
+    SerdeError(#[fail(cause)] serde_json::Error),
+}
+
+impl std::convert::From<serde_json::Error> for MainError {
+    fn from(e: serde_json::Error) -> MainError {
+        MainError::SerdeError(e)
+    }
+}
+
+fn main() -> Result<(), MainError> {
+    let c = (0..3).map(|i| i + 1).collect::<Vec<i32>>();
+    let m = (0..3)
+        .enumerate()
+        .map(|(u, i)| (u, i.to_string()))
+        .collect::<HashMap<usize, String>>();
+    println!("{:?}", c);
+    println!("{:?}", m);
+    let mymap = MyMap {
+        inner_map: (0..3)
+            .enumerate()
+            .map(|(u, i)| (u as i32, i.to_string()))
+            .collect(),
+    };
+    println!("{:?}", mymap);
+    let s = serde_json::to_string(&mymap)?;
+    println!("{}", s);
+    //let s = s.trim_end_matches("}");
+    let b: MyMap = serde_json::from_str(&s)?;
+    println!("{:?}", mymap);
+
+    Ok(())
 }
